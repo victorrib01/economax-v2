ae02204e5335676b7b3954f0075a5ae638cba3dd
Victor Ribeiro <vvictorrib@gmail.com>
2023-07-08 18:24:19 -0300

    trying fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+import tsconfigPaths from "vite-tsconfig-paths";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "",
+  plugins: [react(), tsconfigPaths(), EnvironmentPlugin({ USE_MSW: "" })],
+});
